b28c306c5de070d566e859bdff31a0a85dbc7fcc
Joshua Morris <php.rack@gmail.com>
2011-03-30 04:20:58 -0400

    Prb_I_ReadableStreamlike: updating read signature.

--- lib/prb/i/readablestreamlike.php
+++ lib/prb/i/readablestreamlike.php
@@ -4,7 +4,7 @@
 interface Prb_I_ReadableStreamlike
 {
 	public function gets();
-	public function read( $length = null, $buffer = null );
+	public function read( $length = null, &$buffer = null );
 	public function each( $callback );
 	public function rewind();
 }
